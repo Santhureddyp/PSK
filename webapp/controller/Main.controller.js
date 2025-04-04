8c8c219f06434d55bbeb9471b566154b36f2c992
Santhoshi Patnam <santhoshikumari.n@gmail.com>
2025-04-04 02:11:48 +0000

    Change in I18N

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,10 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], (Controller) => {
+    "use strict";
+
+    return Controller.extend("com.ss.psk.controller.Main", {
+        onInit() {
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/ss/psk/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
